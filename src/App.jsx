32f274d9bd5de8a6124780a76c4b83703bee7995
Tharuu003 <ar113029@fhss.sjp.ac.lk>
2025-09-23 10:25:40 +0530

    Add a footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,8 @@ import Home from './pages/Home.jsx';
 import './App.css';
 
 function App() {
-
   return (
-    <div className='background'>
-      <Navbar />
+    <div>
       <Home />
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -390,7 +390,7 @@ const Footer = () => {
     <footer className="w-full bg-black text-silver text-center py-4 z-40">
       <div className="container mx-auto">
         <p className="text-sm">
-          &copy; ${new Date().getFullYear()} ඔබේ නම. සියලුම හිමිකම් ඇවිරිණි.
+          &copy; {new Date().getFullYear()} Tharushi Kodithuwakku. All rights reserved.
         </p>
       </div>
     </footer>
